6b0921b05b6ac40449e546b8b548d7b5f6910b65
Quentin <QDM@localhost.localdomain>
2019-11-25 14:05:46 +0100

    [UP] src/game_loop.c: cleaning code

--- src/game_loop.c
+++ src/game_loop.c
@@ -56,15 +56,13 @@ int get_x(data **game)
 {
     char *entry = NULL;
     size_t buff = 0;
-    int ret_v;
     int nb[19] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11,
                   12, 13, 14, 15, 16, 17, 18, 19};
 
     (*game)->played_x = 0;
     my_putstr("Line: ");
     while (!(*game)->played_x) {
-        ret_v = getline(&entry, &buff, stdin);
-        if (ret_v == ERROR)
+        if (getline(&entry, &buff, stdin == ERROR))
             return (quit(entry));
         (*game)->played_x = my_getnbr(entry);
         if (!my_int_isinarray(nb, (*game)->played_x, 19))
